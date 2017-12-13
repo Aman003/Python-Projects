d9552095f41fc92f0569697eb1e867e25cc2063b
Aman003 <it17.aman@gmail.com>
2017-12-13 23:52:53 +0530

    Check Git in VS

---
+++ Desktop/blotter-wip/RV.TradeBlotter.Desktop/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RV.TradeBlotter.Desktop
+{
+    class Class1
+    {
+        TradeBlotter;
+    }
+}
